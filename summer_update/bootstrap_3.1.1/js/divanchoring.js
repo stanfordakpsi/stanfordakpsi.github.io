5da6495ebfaf9de821e5076fbc5d46cb2c2b7319
Rotimi Opeke <rotimi.opeke@gmail.com>
2014-09-15 16:18:16 +0100

    Fixed fixedToTop scripts, simplified CSS files, and adjusted corporate logos on Brotherhood page

---
+++ summer_update/bootstrap_3.1.1/js/divanchoring.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+	$('#execucomm').scrollToFixed({limit: 600});
+	$('#activmem').scrollToFixed({limit: $('#chapter-advisor').offset().top - 500});
+	$('#alum').scrollToFixed();
+});
