35c92fb82e1eb70ec9cec596a2d38ede5be79cb8
Rotimi Opeke <rotimi.opeke@gmail.com>
2014-09-18 03:16:54 -0700

    Trying to fix preload js

--- bootstrap_3.1.1/js/preload_images.js
+++ bootstrap_3.1.1/js/preload_images.js
@@ -13,10 +13,10 @@ function preload(arrayOfImages) {
 // Usage:
 
 preload([
-    '../img/spring_2013/test.JPG',
-    '../img/spring_2014/AKPsi_banner_fun.JPG',
-    '../img/spring_2014/napa_tour.JPG',
-    '../img/spring_2014/people_moving.JPG',
-    '../img/spring_2014/sports_day.JPG',
-    '../img/spring_2014/case_interview.JPG'
+    '../bootstrap_3.1.1/img/spring_2013/test.JPG',
+    '../bootstrap_3.1.1/img/spring_2014/AKPsi_banner_fun.JPG',
+    '../bootstrap_3.1.1/img/spring_2014/napa_tour.JPG',
+    '../bootstrap_3.1.1/img/spring_2014/people_moving.JPG',
+    '../bootstrap_3.1.1/img/spring_2014/sports_day.JPG',
+    '../bootstrap_3.1.1/img/spring_2014/case_interview.JPG'
 ]);
