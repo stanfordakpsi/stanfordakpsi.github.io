dd8d2524ec9d8c8843ec5fe3206eb9327116c0ef
Rotimi Opeke <rotimi.opeke@gmail.com>
2014-10-26 15:42:32 -0700

    Started adding modals

--- bootstrap_3.1.1/js/divanchoring.js
+++ bootstrap_3.1.1/js/divanchoring.js
@@ -1,8 +1,4 @@
 $(document).ready(function() {
 	$('#execucomm').scrollToFixed({limit: 600});
 	$('#activmem').scrollToFixed({limit: $('#alum').offset().top - 1500});
-	$('#finance').scrollToFixed();
-	$('#consulting').scrollToFixed();
-	$('#technology').scrollToFixed();
-	$('#non-profit').scrollToFixed();
 });
