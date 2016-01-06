ba8baa8c48474f8e413c795f54ea48a1c94ac1ec
William Huang <whuang6@stanford.edu>
2016-01-05 23:26:55 -0800

    new recruitment schedule

--- bootstrap_3.1.1/js/day_select.js
+++ bootstrap_3.1.1/js/day_select.js
@@ -8,8 +8,8 @@ document.getElementById("friday").onmouseover = function() {Friday() };
 document.getElementById("saturday").onmouseover = function() {Saturday() };
 
 function Monday() {
-  document.getElementById("event-title").innerHTML = "Introduction to AKPsi";
-  document.getElementById("event-details").innerHTML = "5pm-7pm @ Black Community Services Center";
+  document.getElementById("event-title").innerHTML = "Meet the Chapter";
+  document.getElementById("event-details").innerHTML = "5:30pm-7:30pm @ Black Community Services Center";
   document.getElementById("event-description").innerHTML = "Come out and see what Alpha Kappa Psi is all about!" + 
   " We will be introducing the organization <br> and hosting an open networking session right after.";
   document.getElementById("event-div").style.opacity = 0;
@@ -20,10 +20,10 @@ function Monday() {
 
 }
 function Tuesday() {
-  document.getElementById("event-title").innerHTML = "Meet the Brothers";
-  document.getElementById("event-details").innerHTML = "8pm-10pm @ Toyon Main Lounge";
+  document.getElementById("event-title").innerHTML = "Hangout with AKPsi";
+  document.getElementById("event-details").innerHTML = "6:30pm-9:00pm @ TBD";
   document.getElementById("event-description").innerHTML = "Tuesday is a chance to get" +
-  " to know the brothers on a <br> more informal level. Come for the food and stay for the company!";
+  " to know the brothers on a <br> more informal level. Come for the food and games and stay for the company!";
   document.getElementById("event-div").style.opacity = 0;
   pic_obj.style.opacity = 0;
   pic_obj.style.backgroundImage = "url('bootstrap_3.1.1/img/spring_2015/AKPsi_fun.JPG')";
@@ -32,7 +32,7 @@ function Tuesday() {
 }
 function Wednesday() {
   document.getElementById("event-title").innerHTML = "Alumni Night";
-  document.getElementById("event-details").innerHTML = "7pm-8:30pm @ 420-041 (Subject to Change)";
+  document.getElementById("event-details").innerHTML = "5:30pm-7:30pm @ TBD";
   document.getElementById("event-description").innerHTML = "Our Alumni are working all around the world; come" +
    " meet a few that are still in the area <br> and hear about how AKPsi shaped their college experiences!";
   document.getElementById("event-div").style.opacity = 0;
@@ -42,10 +42,9 @@ function Wednesday() {
   window.setTimeout("$('#event-div').animate({opacity: '0.8'}, 1000)", 750);
 }
 function Thursday() {
-  document.getElementById("event-title").innerHTML = "Resume Workshop";
-  document.getElementById("event-details").innerHTML = "7pm-9pm @ Huang Basement";
-  document.getElementById("event-description").innerHTML = "Hone your resume in small group work sessions with" +
-  " our brothers.";
+  document.getElementById("event-title").innerHTML = "Open House";
+  document.getElementById("event-details").innerHTML = "6:30pm-8:30pm @ TBD";
+  document.getElementById("event-description").innerHTML = "From consulting to breakdancing, come check out everything that goes on inside the organization.";
   document.getElementById("event-div").style.opacity = 0;
   pic_obj.style.opacity = 0;
   pic_obj.style.backgroundImage = "url('bootstrap_3.1.1/img/spring_2015/resume_workshop.jpg')";
@@ -53,8 +52,8 @@ function Thursday() {
   window.setTimeout("$('#event-div').animate({opacity: '0.8'}, 1000)", 750);
 }
 function Friday() {
-  document.getElementById("event-title").innerHTML = "Sports Day";
-  document.getElementById("event-details").innerHTML = "5pm-7pm @ Manzanita Field";
+  document.getElementById("event-title").innerHTML = "Field Day";
+  document.getElementById("event-details").innerHTML = "4:30pm-6:30pm @ TBD";
   document.getElementById("event-description").innerHTML = "Like every Stanford student, we love being outside!" +
   " Come play ball with us or join us on the grass!";
   document.getElementById("event-div").style.opacity = 0;
@@ -64,10 +63,10 @@ function Friday() {
   window.setTimeout("$('#event-div').animate({opacity: '0.8'}, 1000)", 750);
 }
 function Saturday() {
-  document.getElementById("event-title").innerHTML = "Case-Study Workshop";
-  document.getElementById("event-details").innerHTML = "9am-1pm @ Huang Basement";
-  document.getElementById("event-description").innerHTML = "Work on real case interview questions with our" +
-  " brothers and learn what it's like <br> to be a consultant.";
+  document.getElementById("event-title").innerHTML = "Product Pitch Workshop";
+  document.getElementById("event-details").innerHTML = "8:00am-2:00pm @ Huang Basement";
+  document.getElementById("event-description").innerHTML = "Work on product pitch with our" +
+  " brothers.";
   document.getElementById("event-div").style.opacity = 0;
   pic_obj.style.opacity = 0;
   pic_obj.style.backgroundImage = "url('bootstrap_3.1.1/img/spring_2015/case_workshop.JPG')";
